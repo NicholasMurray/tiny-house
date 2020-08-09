cac370ff37b549c6049e24709eeac24eb1aae0e4
Nicholas Murray <murray.nicholas@gmail.com>
2020-08-09 22:39:31 +0100

    Added apollo and graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,12 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-// Listings
-app.get("/listings", (req, res) => {
-  res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    const element = listings[i];
-    if (element.id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send("failed to find listing");
-});
-
-// Delete Listings
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
